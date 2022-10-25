7e9fe777f9f8aa040edc4314ce6906b3ef805682
Nobody <acc.4code@gmail.com>
2022-10-25 21:25:08 +0800

    Update moveProxiesToSync.sh

--- moveProxiesToSync.sh
+++ moveProxiesToSync.sh
@@ -1,8 +1,6 @@
 moveProxiesToSync() {
   proxies=()
   begin=0
-  cat $1
-  cat $2
   while read -r line; do
     echo $line
     if [[ $begin == 1 ]]; then
@@ -13,7 +11,6 @@ moveProxiesToSync() {
       fi
     fi
     if [[ $line =~ "^proxies:" ]]; then
-      echo 123123123123123
       begin=1
     fi
   done < "$1"
