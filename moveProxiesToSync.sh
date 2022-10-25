a63f15a6d4230cc11dd591e4fade5ce799bb6b3d
Nobody <acc.4code@gmail.com>
2022-10-25 21:33:19 +0800

    Update moveProxiesToSync.sh

--- moveProxiesToSync.sh
+++ moveProxiesToSync.sh
@@ -3,14 +3,13 @@ moveProxiesToSync() {
   begin=0
   while read -r line; do
     if [[ $begin == 1 ]]; then
-      if [[ $line =~ "^[ ]*-.*$" ]]; then
+      if [[ $line =~ "  - {" ]]; then
         proxies+=("$line")
       else
         break;
       fi
     fi
-    if [[ $line =~ "^[ ]*proxies:" ]]; then
-      echo $line
+    if [[ $line =~ "proxies:" ]]; then
       begin=1
     fi
   done < "$1"
